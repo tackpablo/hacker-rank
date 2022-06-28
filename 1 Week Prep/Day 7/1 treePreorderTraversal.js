fca852f9f17dc9ea8e8ed280cc66fad6a1107452
Pablo <tackpablo@gmail.com>
2022-06-28 07:17:18 -0700

    Added solutions to day 7 question 1

---
+++ 1 Week Prep/Day 7/1 treePreorderTraversal.js
@@ -0,0 +1,7 @@
+/////////////////////////
+// https://www.hackerrank.com/challenges/one-week-preparation-kit-tree-preorder-traversal/problem?h_l=interview&isFullScreen=true&playlist_slugs%5B%5D%5B%5D=preparation-kits&playlist_slugs%5B%5D%5B%5D=one-week-preparation-kit&playlist_slugs%5B%5D%5B%5D=one-week-day-seven
+/////////////////////////
+
+// Complete the preOrder function in the editor below, which has l parameter: a pointer to the root of a binary tree. It must print the values in the tree's preorder traversal as a single line of space-separated values.
+
+// language not avaiable in Javascript
